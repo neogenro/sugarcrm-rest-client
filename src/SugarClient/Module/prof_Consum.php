daced324e6cf07a759b09494c095ef45c515c51c
Pintea Tibi <tibi@neogen.biz>
2016-10-14 08:20:51 +0300

    create prof_Consum module
    For importing consums in brain

---
+++ src/SugarClient/Module/prof_Consum.php
@@ -0,0 +1,14 @@
+<?php
+namespace SugarClient\Module;
+
+use SugarClient\Core\Module;
+
+class prof_Consum extends Module
+{
+   public function __construct($attributes = array())
+    {
+        parent::__construct(array(
+            'attributes' => $attributes
+        ));
+    }
+}
